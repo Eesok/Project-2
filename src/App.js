dcb5bef54b4132a0a4e8633a979eba222d7dac0a
isaac93romano@gmail.com <isaac93romano@gmail.com>
2020-07-29 14:23:51 -0400

    make it better

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Country from './Country';
+import CountryList from './CountryList';
+import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { createClient } from 'pexels';
 
-function App() {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+const client = createClient(
+	'563492ad6f917000010000010dfc1943a0ad41d0a6d1e95c490aed47'
+);
+
+// All requests made with the client will be authenticated
+
+const url = 'https://api.pexels.com/v1';
+// const key = '563492ad6f917000010000010dfc1943a0ad41d0a6d1e95c490aed47';
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	componentDidMount() {
+		axios(url, {
+			header: {
+				Authorization: `Basic ${client}`,
+			},
+		})
+			.then((response) => {
+        console.log(response);
+      })
+			.catch(console.error);
+	}
+	render() {
+		return <div></div>;
+	}
 }
 
 export default App;
