ad112ec8cb64a1ae1e9faba65879adef74538d42
isaac93romano@gmail.com <isaac93romano@gmail.com>
2020-08-03 20:39:32 -0400

    completed the About page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
-// import Country from './Country';
 import CountryList from './CountryList';
 import { Route, Redirect } from 'react-router-dom';
 import React, { Component } from 'react';
-// import { createClient } from 'pexels';
 import Axios from 'axios';
 import CountryImages from './CountryImages';
 import Search from './Search';
 import About from './About';
 
-// const client = createClient(
-// 	'563492ad6f917000010000010dfc1943a0ad41d0a6d1e95c490aed47'
-// );
 
 class App extends Component {
 	constructor(props) {
